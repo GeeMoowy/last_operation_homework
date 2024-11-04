ddf213575cdf1359d0de75bda1ccf3ee79b7041a
Вячеслав Манютин <kovylek.ul@mail.ru>
2024-11-04 16:57:44 +0300

    add func get date and scan of linters

--- src/widget.py
+++ src/widget.py
@@ -2,14 +2,19 @@ import masks
 
 
 def mask_account_card(card_info: str) -> str:
+    """Функция принимает строку с информацией о карте и возвращает строку с маской номера"""
     card_info_list = card_info.split()
     card_number = int(card_info_list[-1])
-    if card_info_list[0] == 'Счет':
+    if card_info_list[0] == "Счет":
         mask_number = masks.get_mask_account(card_number)
-        return card_info_list[0] + " " + mask_number
+        return str(card_info_list[0] + " " + mask_number)
     else:
         mask_number = masks.get_mask_card_number(card_number)
-        return " ".join(card_info_list[:-1]) + " " + mask_number
+        return str(" ".join(card_info_list[:-1]) + " " + mask_number)
 
 
-print(mask_account_card('Visa Gold 12354886532111123546'))
+def get_date(my_date: str) -> str:
+    """Функция принимает строку с датой и переформатирует в 'ДД.ММ.ГГГГ'"""
+    date_list = my_date[:10].split("-")
+    year, month, day = date_list[0], date_list[1], date_list[2]
+    return f'"{day}.{month}.{year}"'
