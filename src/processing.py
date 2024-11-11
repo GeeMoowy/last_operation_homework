d8e2ba093a26dc1e7bca35bc58ea8ef730a9d39e
Вячеслав Манютин <kovylek.ul@mail.ru>
2024-11-11 23:01:37 +0300

    add function sort_by_date in processing.py

--- src/processing.py
+++ src/processing.py
@@ -10,3 +10,10 @@ def filter_by_state(list_of_operations: List[dict], state_value: str ="EXECUTED"
         if operation["state"] == state_value:
             filter_list.append(operation)
     return filter_list
+
+
+def sort_by_date(list_of_operations: List[dict], sorting_order: bool =True) -> List[dict]:
+    """Функция принимает список словарей с банковскими операциями
+    и возвращает отсортированный список словарей по дате в порядке убывания"""
+    sorted_list = sorted(list_of_operations, key=lambda x: x["date"], reverse=sorting_order)
+    return sorted_list
