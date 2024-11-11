d28470dd6213e308cdadeac2a7c954b034886015
Вячеслав Манютин <kovylek.ul@mail.ru>
2024-11-12 00:23:58 +0300

    formated by linters

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,7 @@
 from typing import List
 
 
-def filter_by_state(list_of_operations: List[dict], state_value: str ="EXECUTED") -> List[dict]:
+def filter_by_state(list_of_operations: List[dict], state_value: str = "EXECUTED") -> List[dict]:
     """Функция принимает список словарей с банковскими операциями
     и возвращает отфильтрованный список словарей со значением по ключу
     'state' = 'EXECUTED'(состояние = ВЫПОЛНЕНО)"""
@@ -12,7 +12,7 @@ def filter_by_state(list_of_operations: List[dict], state_value: str ="EXECUTED"
     return filter_list
 
 
-def sort_by_date(list_of_operations: List[dict], sorting_order: bool =True) -> List[dict]:
+def sort_by_date(list_of_operations: List[dict], sorting_order: bool = True) -> List[dict]:
     """Функция принимает список словарей с банковскими операциями
     и возвращает отсортированный список словарей по дате в порядке убывания"""
     sorted_list = sorted(list_of_operations, key=lambda x: x["date"], reverse=sorting_order)
