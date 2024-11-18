2dd25c684375e64f5b22d122b50a6e503ce4e8e2
Вячеслав Манютин <kovylek.ul@mail.ru>
2024-11-18 23:16:40 +0300

    correction by linters

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,5 +1,6 @@
 import pytest
-from src.widget import mask_account_card, get_date
+
+from src.widget import get_date, mask_account_card
 
 
 @pytest.mark.parametrize("card_info, mask_card", [
@@ -7,9 +8,9 @@ from src.widget import mask_account_card, get_date
     ("Счет 64686473678894779589", "Счет **9589"),
     ("MasterCard 7158300734726758", "MasterCard 7158 30** **** 6758")
 ])
-def test_mask_account_card(card_info, mask_card):
+def test_mask_account_card(card_info: str, mask_card: str) -> None:
     assert mask_account_card(card_info) == mask_card
 
 
-def test_get_date(full_date):
+def test_get_date(full_date: str) -> None:
     assert get_date(full_date) == "11.03.2024"
