6de04b065096f543a1e1526e8d0aaa1cb51bb5cd
Вячеслав Манютин <kovylek.ul@mail.ru>
2024-11-29 00:15:59 +0300

    modified README.md, check by linters, update htmlcov

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,42 +1,43 @@
-import pytest
-
 from typing import Any
 
-from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
+import pytest
+
+from src.generators import card_number_generator, filter_by_currency, transaction_descriptions
 
 
 def test_filter_by_currency(list_of_transactions: list[dict[str, Any]], my_currency: str) -> None:
     generator = filter_by_currency(list_of_transactions, my_currency)
     assert next(generator) == {
-            "id": 939719570,
-            "state": "EXECUTED",
-            "date": "2018-06-30T02:08:58.425572",
-            "operationAmount": {
-                "amount": "9824.07",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод организации",
-            "from": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702"
-        }
+        "id": 939719570,
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+        "operationAmount": {
+            "amount": "9824.07",
+            "currency": {
+                "name": "USD",
+                "code": "USD"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "Счет 75106830613657916952",
+        "to": "Счет 11776614605963066702"
+    }
     assert next(generator) == {
-            "id": 142264268,
-            "state": "EXECUTED",
-            "date": "2019-04-04T23:20:05.206878",
-            "operationAmount": {
-                "amount": "79114.93",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 19708645243227258542",
-            "to": "Счет 75651667383060284188"
-        }
+        "id": 142264268,
+        "state": "EXECUTED",
+        "date": "2019-04-04T23:20:05.206878",
+        "operationAmount": {
+            "amount": "79114.93",
+            "currency": {
+                "name": "USD",
+                "code": "USD"
+            }
+        },
+        "description": "Перевод со счета на счет",
+        "from": "Счет 19708645243227258542",
+        "to": "Счет 75651667383060284188"
+    }
+
 
 def test_filter_by_currency_empty() -> None:
     with pytest.raises(StopIteration):
@@ -62,10 +63,10 @@ def test_transaction_descriptions_empty() -> None:
             "0000 0000 0000 0004",
             "0000 0000 0000 0005"]
      ),
-     (9999999999999998, 9999999999999999,
-            ["9999 9999 9999 9998",
-            "9999 9999 9999 9999"]
-      )]
+    (9999999999999998, 9999999999999999,
+     ["9999 9999 9999 9998",
+      "9999 9999 9999 9999"]
+     )]
                          )
 def test_card_number_generator(start: int, finish: int, result: str) -> None:
     gen = card_number_generator(start, finish)
