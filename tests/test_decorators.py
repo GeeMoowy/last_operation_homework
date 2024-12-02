8c75159f1e1b7db2d6468779eedbb3ab778c4d99
Вячеслав Манютин <kovylek.ul@mail.ru>
2024-12-03 01:03:11 +0300

    check by linters

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -5,7 +5,7 @@ def test_log(capsys):
     @log(filename=None)
     def my_func(x, y):
         return x + y
-    my_func(1,2)
+    my_func(1, 2)
     captured = capsys.readouterr()
     assert captured.out == "my_func OK\n\n"
 
@@ -14,7 +14,7 @@ def test_log_err(capsys):
     @log(filename=None)
     def my_func(x, y):
         return x + y
-    my_func("1",2)
+    my_func("1", 2)
     captured = capsys.readouterr()
     assert captured.out == "my_func error: can only concatenate str (not \"int\") to str. Inputs ('1', 2), {} \n\n"
 
@@ -23,5 +23,5 @@ def test_log_positive():
     @log(filename="my_log")
     def my_func(x, y):
         return x + y
-    captured = my_func(1,2)
+    captured = my_func(1, 2)
     assert captured == 3
