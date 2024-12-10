750b39d38b7bc51eff46e54c954275d5de541484
Вячеслав Манютин <kovylek.ul@mail.ru>
2024-12-11 01:00:32 +0300

    add tests for utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,35 +1,28 @@
 import json
 import os
 from json import JSONDecodeError
-from pathlib import Path
 from typing import Any
 import requests
 from dotenv import load_dotenv
-
-
-PATH_TO_DIR = Path(__file__).parent.parent
-PATH_TO_FILE = Path(PATH_TO_DIR, "data", "operations.json")
 
 
 load_dotenv()
 apilayer_key = os.getenv("API_KEY")
 
 
-def transactions_list(file_path: Path) -> list[dict[str, Any]]:
+def transactions_list(file_path: str) -> list[dict[str, Any]]:
     if os.path.isfile(file_path):
         with open(file_path, "r", encoding="utf-8") as json_file:
             try:
                 transactions = json.load(json_file)
             except JSONDecodeError:
                 return []
-            except FileNotFoundError:
-                return []
         return transactions
     else:
         return []
 
 
-def currency_conversion(transaction: dict[str, Any]) -> float:
+def amount_transaction(transaction: dict[str, Any]) -> float:
     if transaction["operationAmount"]["currency"]["code"] != "RUB":
         url = "https://api.apilayer.com/exchangerates_data/convert"
         payload = {
@@ -42,22 +35,5 @@ def currency_conversion(transaction: dict[str, Any]) -> float:
         }
         response = requests.get(url, headers=headers, params=payload)
         result = response.json()
-        return result['result']
+        return float(result['result'])
     return float(transaction["operationAmount"]["amount"])
-
-
-print(currency_conversion({
-    "id": 41428829,
-    "state": "EXECUTED",
-    "date": "2019-07-03T18:35:29.512364",
-    "operationAmount": {
-      "amount": "8221.37",
-      "currency": {
-        "name": "USD",
-        "code": "USD"
-      }
-    },
-    "description": "Перевод организации",
-    "from": "MasterCard 7158300734726758",
-    "to": "Счет 35383033474447895560"
-  }))
---
+++ tests/test_utils.py
@@ -0,0 +1,39 @@
+from urllib import response
+
+import pytest
+from unittest.mock import patch, mock_open, Mock
+import json
+
+import requests
+
+from src.utils import transactions_list, amount_transaction
+
+
+@patch("builtins.open", new_callable=mock_open, read_data='[{"id": 441945886}]')
+def test_transactions_list(mock_file):
+    transactions = transactions_list("data/operations.json")
+    assert transactions == [{"id": 441945886}]
+
+
+@patch("builtins.open", new_callable=mock_open, read_data='')
+def test_transactions_list_empty(mock_file):
+    transactions = transactions_list("data/operations.json")
+    assert transactions == []
+
+
+@patch("builtins.open", new_callable=mock_open, read_data='{"id": 441945886}]')
+def test_transactions_list_incorrect(mock_file):
+    transactions = transactions_list("data/operations.json")
+    assert transactions == []
+
+
+def test_amount_transaction_rub(my_transaction_rub):
+    assert amount_transaction(my_transaction_rub) == 31957.58
+
+res = {'success': True, 'query': {'from': 'USD', 'to': 'RUB', 'amount': 8221.37}, 'info': {'timestamp': 1733867463, 'rate': 103.045091}, 'date': '2024-12-10', 'result': 847171.819795}
+
+
+@patch('requests.get')
+def test_amount_transaction_usd(mock_get, my_transaction_usd):
+    mock_get.return_value.json.return_value = res
+    assert amount_transaction(my_transaction_usd) == 847171.819795
