361e668a736c24c774f06cf8d32b777d8acb9a1d
Вячеслав Манютин <kovylek.ul@mail.ru>
2024-12-10 13:20:13 +0300

    create functions transactions_list and currency_conversion in utils.py

--- src/utils.py
+++ src/utils.py
@@ -3,10 +3,16 @@ import os
 from json import JSONDecodeError
 from pathlib import Path
 from typing import Any
+import requests
+from dotenv import load_dotenv
 
 
 PATH_TO_DIR = Path(__file__).parent.parent
 PATH_TO_FILE = Path(PATH_TO_DIR, "data", "operations.json")
+
+
+load_dotenv()
+apilayer_key = os.getenv("API_KEY")
 
 
 def transactions_list(file_path: Path) -> list[dict[str, Any]]:
@@ -21,3 +27,37 @@ def transactions_list(file_path: Path) -> list[dict[str, Any]]:
         return transactions
     else:
         return []
+
+
+def currency_conversion(transaction: dict[str, Any]) -> float:
+    if transaction["operationAmount"]["currency"]["code"] != "RUB":
+        url = "https://api.apilayer.com/exchangerates_data/convert"
+        payload = {
+            "amount": transaction["operationAmount"]["amount"],
+            "from": transaction["operationAmount"]["currency"]["code"],
+            "to": "RUB"
+        }
+        headers = {
+            "apikey": f"{apilayer_key}"
+        }
+        response = requests.get(url, headers=headers, params=payload)
+        result = response.json()
+        return result['result']
+    return float(transaction["operationAmount"]["amount"])
+
+
+print(currency_conversion({
+    "id": 41428829,
+    "state": "EXECUTED",
+    "date": "2019-07-03T18:35:29.512364",
+    "operationAmount": {
+      "amount": "8221.37",
+      "currency": {
+        "name": "USD",
+        "code": "USD"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "MasterCard 7158300734726758",
+    "to": "Счет 35383033474447895560"
+  }))
