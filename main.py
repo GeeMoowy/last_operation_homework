ab48339fd767f67054df021ef8af033b8d79acee
geemoowy <kovylek.ul@gmail.com>
2024-12-17 22:37:59 +0300

    check for linters

--- main.py
+++
@@ -1,4 +0,0 @@
-from src.reading_file import get_reading_csv, get_reading_exel
-
-#print(get_reading_csv('C:/Users/USER/PycharmProjects/last_operation_homework/data/transactions.csv'))
-print(get_reading_exel('C:/Users/USER/PycharmProjects/last_operation_homework/tests/test_exel.xlsx'))
--- tests/test_reading_files.py
+++ tests/test_reading_files.py
@@ -1,14 +1,11 @@
 from unittest.mock import patch, mock_open
-
 import pandas as pd
-import pytest
-
-import src
 from src.reading_file import get_reading_csv, get_reading_exel
 
 
 def test_get_reading_csv():
-    mock_data = "id;state;date;amount\n650703;EXECUTED;2023-09-05T11:30:32Z;16210\n3598919;EXECUTED;2020-12-06T23:00:58Z;29740"
+    mock_data = ("id;state;date;amount\n650703;EXECUTED;"
+                 "2023-09-05T11:30:32Z;16210\n3598919;EXECUTED;2020-12-06T23:00:58Z;29740")
     expected = [
         {'id': '650703', 'state': 'EXECUTED', 'date': '2023-09-05T11:30:32Z', 'amount': '16210'},
         {'id': '3598919', 'state': 'EXECUTED', 'date': '2020-12-06T23:00:58Z', 'amount': '29740'}
