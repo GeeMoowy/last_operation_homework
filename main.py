e8040f6a3d077d83cf983a2fe5782749fc9eea38
geemoowy <kovylek.ul@gmail.com>
2024-12-17 22:10:25 +0300

    add tests for reading_files

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from src.reading_file import get_reading_csv, get_reading_exel
 
-print(get_reading_csv('C:/Users/USER/PycharmProjects/last_operation_homework/data/transactions.csv'))
-#print(get_reading_exel('C:/Users/USER/PycharmProjects/last_operation_homework/data/transactions_excel.xlsx'))
+#print(get_reading_csv('C:/Users/USER/PycharmProjects/last_operation_homework/data/transactions.csv'))
+print(get_reading_exel('C:/Users/USER/PycharmProjects/last_operation_homework/tests/test_exel.xlsx'))
