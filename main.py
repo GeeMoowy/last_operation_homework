4baca90b401e041ede9f93668f578c5756aefb82
geemoowy <kovylek.ul@gmail.com>
2024-12-17 09:55:10 +0300

    add logging in reading_files

---
+++ main.py
@@ -0,0 +1,4 @@
+from src.reading_file import get_reading_csv, get_reading_exel
+
+print(get_reading_csv('C:/Users/USER/PycharmProjects/last_operation_homework/data/transactions.csv'))
+#print(get_reading_exel('C:/Users/USER/PycharmProjects/last_operation_homework/data/transactions_excel.xlsx'))
--- src/reading_file.py
+++ src/reading_file.py
@@ -1,16 +1,33 @@
 import csv
+import logging
 from pathlib import Path
 from typing import Union, Any
-
 import pandas as pd
 
 
+logger = logging.getLogger('reading_files')
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler('logs/reading_files.log', 'w', encoding='UTF-8')
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
+
 def get_reading_csv(path_to_csv: Union[str, Path]) -> list[dict[str, Any]]:
-    with open(path_to_csv, encoding='UTF-8') as csv_file:
-        reader_csv = csv.DictReader(csv_file, delimiter=';')
-        for row in reader_csv:
+    try:
+        logger.info(f'Читаем файл: {path_to_csv}')
+        with open(path_to_csv, encoding='UTF-8') as csv_file:
+            reader_csv = csv.DictReader(csv_file, delimiter=';')
             data = [row for row in reader_csv]
-    return data
+            if not data:
+                print('Файл пуст или содержит не корректные данные')
+                logger.info('Файл пустой или не верные данные')
+            logger.info(f'Функция {get_reading_csv.__name__} отработала корректно.')
+            return data
+    except FileNotFoundError as e:
+        logger.info(f'Файл не найден с ошибкой {e}')
+        return []
+
 
 
 def get_reading_exel(path_to_exel: Union[str, Path]) -> list[dict[str, Any]]:
@@ -18,5 +35,3 @@ def get_reading_exel(path_to_exel: Union[str, Path]) -> list[dict[str, Any]]:
     dict_list = df.to_dict(orient='records')
     return dict_list
 
-#print(get_reading_csv('C:/Users/USER/PycharmProjects/last_operation_homework/data/transactions.csv'))
-#print(get_reading_exel('C:/Users/USER/PycharmProjects/last_operation_homework/data/transactions_excel.xlsx'))
